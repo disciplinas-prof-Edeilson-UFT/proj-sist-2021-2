64fc3a0ce418192064bc5216e3b62fcd23050029
Daniel de Jesus Guimarães <66970708+djesusguimaraes@users.noreply.github.com>
2021-09-27 18:13:34 -0300

    :sparckles: Adiciona Classes do carrinho de compras

---
+++ lib/app/global/models/entities/item.dart
@@ -0,0 +1,11 @@
+import 'package:pscomidas/app/global/models/entities/product.dart';
+
+class Item {
+  final Product product;
+  final int quantidade;
+
+  Item(
+    this.product,
+    this.quantidade,
+  );
+}
---
+++ lib/app/global/models/entities/product.dart
@@ -0,0 +1,19 @@
+class Product {
+  final bool available;
+  final List<String> categories;
+  final String description;
+  final String imgUrl;
+  final String name;
+  final double price;
+  final String restaurantId;
+
+  Product(
+    this.available,
+    this.categories,
+    this.description,
+    this.imgUrl,
+    this.name,
+    this.price,
+    this.restaurantId,
+  );
+}
