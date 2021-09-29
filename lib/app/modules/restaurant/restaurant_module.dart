73272062eab685e1da5585fc12e53203dd5d0e18
Daniel de Jesus Guimarães <66970708+djesusguimaraes@users.noreply.github.com>
2021-09-29 00:57:56 -0300

    Adiciona componente `CustomAppBar` com LayoutBulder

--- lib/app/modules/restaurant/restaurant_module.dart
+++ lib/app/modules/restaurant/restaurant_module.dart
@@ -3,6 +3,7 @@ import 'package:pscomidas/app/modules/restaurant/restaurant_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class RestaurantModule extends Module {
+  static String get routeName => '/restaurant';
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => RestaurantStore()),
