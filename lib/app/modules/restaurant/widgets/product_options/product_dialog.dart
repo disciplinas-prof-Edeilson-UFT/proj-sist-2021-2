a933d7f9a90b63df001b895dfc382b845ff5d128
Max Alexandre <max.alexandre839@gmail.com>
2021-10-08 22:03:21 -0300

    Correção de layout em `product_dialog.dart`

--- lib/app/modules/restaurant/widgets/product_options/product_dialog.dart
+++ lib/app/modules/restaurant/widgets/product_options/product_dialog.dart
@@ -16,27 +16,33 @@ class ProductDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     // Size screen = MediaQuery.of(context).size;
     final Product product = Product(
-      description: "Cremoso sorvete sabor torta de limão com chip de biscoito e cobertura chocolate branco com raspas de limão!",
-      name: 'EXTRUSADO TORTA DE LIMÃO',
-      price: 10.0,
+      description:
+          "Combo Prime! Acompanha: 10 Sashimis de salmão, 10 Camarões empanados, 36 Hots Philadelphia, 16 Enrolados de salmão cream cheese e cebolinha, 8 Enrolados salmão skin cream cheese e cebolinha, 4 Niguis de salmão com cream cheese e cebolinha, 4 Niguis skin loko, 10 Jyos de salmão cream cheese e 10 Salmão cubes (108 peças). Imagens meramente ilustrativas. Este combo adiciona 25 min no tempo de entrega.\n\nEscolha com consciência se deseja incluir ou não seu Kit delivery.\n\nItens adicionais colocados nas observações não acompanharão o pedido.",
+      name: 'COMBO PRIME - 108 PEÇAS',
+      price: 184.00,
     );
 
     return AlertDialog(
       titleTextStyle: const TextStyle(color: Colors.black54),
-      title: SizedBox(
-        width: double.infinity,
-        child: Container(
-          alignment: const Alignment(0.55,0.00),
-          color: Colors.white,
-          child: Text(
-            product.name.toString(),
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 13
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Spacer(flex:6),
+           Text(
+              product.name.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+            ),
+          const Spacer(flex:2),
+          GestureDetector(
+            onTap: () {
+              Modular.to.pop();
+              store.dispose();
+            },
+            child: const Align(
+              child: Icon(Icons.close, color: Colors.grey),
             ),
           ),
-        ),
+        ],
       ),
 
       content: Row(
@@ -45,99 +51,85 @@ class ProductDialog extends StatelessWidget {
           Container(
             height: 500.0,
             width: 500.0,
-            decoration: const BoxDecoration(
-               image: DecorationImage(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.red),
+              image: const DecorationImage(
                 fit: BoxFit.fill,
                 image: AssetImage("logos/ifood-logo.png"),
-               ),
+              ),
             ),
           ),
-
-         Container(
-           padding: EdgeInsets.all(10),
-           width: 400,
-           child: ListView(
-             children: [
-               if (product.description!= null) ...[
-                 Container(
-                   color: Colors.white,
-                   width: 500,
-                   alignment: Alignment.bottomLeft,
-                   child: Text(
-                     product.description.toString(),
-                     textAlign: TextAlign.justify,
-                     style: const TextStyle(
-                       color: Colors.grey,
-                         fontSize: 14
-                     ),
-                   ),
-                 ),
-               ],
-               const SizedBox(height: 20),
-
-               if (product.price != null) ...[
-                 Container(
-                   color: Colors.white,
-                   height: 20,
-                   width: 500,
-                   alignment: Alignment.bottomLeft,
-                   child: Text("R\$ "+
-                     product.price.toString(),
-                     textAlign: TextAlign.justify,
-                     style: const TextStyle(
-                       color: Colors.green,
-                       fontSize: 14
-                     ),
-                   ),
-                 ),
-               ],
-               const SizedBox(height: 20),
-               Padding(
-                 padding: const EdgeInsets.all(8.0),
-                 child: Container(
-                   width: 500,
-                   alignment: Alignment.bottomLeft,
-                   child: const Text(
-                     "Algum comentário?",
-                     style: TextStyle(
-                       fontWeight: FontWeight.bold,
-                       color: Color(0xff6F6F67),
-                       fontSize: 16,
-                     ),
-                   ),
-                 ),
-               ),
-               Container(
-                 color: Colors.white,
-                 width: 500,
-                 height: 140,
-                 child: const TextField(
-                   textInputAction: TextInputAction.newline,
-                   autofocus: true,
-                   maxLength: 150,
-                   maxLines: 4,
-                   decoration: InputDecoration(
-                     hintText: "Ex: tirar cebola, maionese à parte, etc.",
-                     border: OutlineInputBorder(),
-                   ),
-                 ),
-               ),
-             ],
-           ),
-         ),
-          GestureDetector(
-            onTap: () {
-              Modular.to.pop();
-              store.dispose();
-            },
-            child: const Align(
-              child: Icon(Icons.close, color: Colors.grey),
-              alignment: Alignment.topRight,
+          const SizedBox(
+            width: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            width: 700,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                if (product.description != null) ...[
+                  Container(
+                    color: Colors.white,
+                    width: 500,
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      product.description.toString(),
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                  ),
+                ],
+                const SizedBox(height: 20),
+                if (product.price != null) ...[
+                  Container(
+                    color: Colors.white,
+                    height: 20,
+                    width: 500,
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "R\$ " + product.price.toString(),
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(color: Colors.green, fontSize: 14),
+                    ),
+                  ),
+                ],
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 500,
+                    alignment: Alignment.bottomLeft,
+                    child: const Text(
+                      "Algum comentário?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff6F6F67),
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 500,
+                  height: 140,
+                  child: const TextField(
+                    textInputAction: TextInputAction.newline,
+                    autofocus: true,
+                    maxLength: 150,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      hintText: "Ex: tirar cebola, maionese à parte, etc.",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
       ),
-      actionsAlignment: MainAxisAlignment.center,
       actions: [
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -150,10 +142,11 @@ class ProductDialog extends StatelessWidget {
                 ),
                 borderRadius: BorderRadius.circular(4),
               ),
-
               child: Row(
                 children: [
                   IconButton(
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
                     onPressed: () => store.decrement(),
                     icon: const Icon(
                       Icons.remove,
@@ -171,6 +164,8 @@ class ProductDialog extends StatelessWidget {
                     ),
                   ),
                   IconButton(
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
                     onPressed: () => store.increment(),
                     icon: const Icon(
                       Icons.add,
@@ -180,11 +175,12 @@ class ProductDialog extends StatelessWidget {
                 ],
               ),
             ),
+
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: SizedBox(
                 height: 55,
-                width: 150,
+                width: 200,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     primary: Colors.red,
@@ -192,14 +188,13 @@ class ProductDialog extends StatelessWidget {
                   ),
                   onPressed: () {},
                   child: Observer(builder: (_) {
-                    return Row(
-                      children: [
-                        Text("Adicionar R\$ " + (product.price! * store.quantity).toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.0,
-                          ),),
-                      ],
+                    return Text(
+                      "Adicionar R\$ " +
+                          (product.price! * store.quantity).toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                      ),
                     );
                   }),
                 ),
