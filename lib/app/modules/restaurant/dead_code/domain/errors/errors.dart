e19a4e777a630583c9bd846905ba832d7433da8e
Daniel de Jesus Guimarães <66970708+djesusguimaraes@users.noreply.github.com>
2021-10-10 19:04:51 -0300

    Remove pasta `dead_code` com implementações do `dartz`

--- lib/app/modules/restaurant/dead_code/domain/errors/errors.dart
+++
@@ -1,3 +0,0 @@
-abstract class SearchError implements Exception {}
-
-class DataSourceError implements SearchError {}
--- lib/app/modules/restaurant/dead_code/domain/repository/search_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:pscomidas/app/modules/restaurant/dead_code/domain/entities/result_search.dart';
-import 'package:pscomidas/app/modules/restaurant/dead_code/domain/errors/errors.dart';
-
-abstract class SearchRepository {
-  Future<Either<SearchError, List<ResultSearch>>> search(String searchTitle);
-}
