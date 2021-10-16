9872c40dec1654091e38eca5b973d7827040747c
alineralncs <aline.rose@mail.uft.edu.br>
2021-10-16 16:53:53 -0300

    R08/T04/feature/plans | Criação dos cards
    - Criação dos cards com responsividade

---
+++ lib/app/modules/register/restaurant/components/register_plans.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class RegisterPlans extends StatelessWidget {
+  const RegisterPlans({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: SingleChildScrollView(
+          controller: ScrollController(initialScrollOffset: 0.0),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Card(
+                  image: "assets/images/cards/whitebackground.png",
+                  colorBox: Colors.white),
+              Card(
+                  image: "assets/images/cards/redbackground.png",
+                  colorBox: Colors.red.shade900),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Card extends StatefulWidget {
+  const Card({Key? key, required this.image, this.colorBox}) : super(key: key);
+  final String image;
+  final colorBox;
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400.0,
+      height: 500.0,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: widget.colorBox,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 7.0, // soften the shadow
+                      spreadRadius: 5.0, //extend the shadow
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: FittedBox(
+                  child: Image.asset(
+                    widget.image,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
