f83393af2a003ad69b240b29bc3fa2fd8ffb3b86
Otavio <silvio76otavio@gmail.com>
2021-10-21 13:16:31 -0300

    R06|T01| feature/Track_order |add info order

--- lib/app/modules/order/bodyOrder_page/body_order.dart
+++ lib/app/modules/order/bodyOrder_page/body_order.dart
@@ -20,19 +20,21 @@ class _BodyOrderState extends State<BodyOrder> {
 
   bool _visible = false;
   void _setVisible() {
-    if (_visible == false) {
-      _visible = true;
-    } else {
-      _visible = false;
-    }
+    setState(() {
+      if (_visible == false) {
+        _visible = true;
+      } else {
+        _visible = false;
+      }
+    });
   }
 
+  /// Pedido que será adicionado, apenas para testes pode [DELETAR] quando terminar.
   OrdersList data = OrdersList(
-    restaurante: 'MacDonalds',
-    id: '9999',
-    previsao: '15:30',
-    status: 'Em preparo'
-  );
+      restaurante: 'MacDonalds',
+      id: '9999',
+      previsao: '15:30',
+      status: 'Em preparo');
 
   @override
   Widget build(BuildContext context) {
@@ -53,23 +55,28 @@ class _BodyOrderState extends State<BodyOrder> {
                       fontFamily: 'Nunito'),
                 );
               }),
-              /*Observer(builder: (_) {
+
+              /// Botão para adicionar um card [DELETAR], serve apenas para testes
+              Observer(builder: (_) {
                 return ElevatedButton(
                     onPressed: () {
                       store.incrementOrder(data);
                     },
                     child: const Text('data'));
-              }),*/  /// TODo Botão para adicionar um card [DELETAR]
+              }),
+
+
               SizedBox(
                 height: screen.height * 0.01,
               ),
 
               // Retorna 2 Widgets, onde um é para caso não tenha pedidos e o outro caso tenha.
-              Observer(builder: (_) {
-                 return  store.order.isEmpty
-                    ?  _emptyOrder()
-                    : _listViewOrder(context);
-              },
+              Observer(
+                builder: (_) {
+                  return store.order.isEmpty
+                      ? _emptyOrder()
+                      : _listViewOrder(context);
+                },
               )
             ],
           ),
@@ -78,19 +85,19 @@ class _BodyOrderState extends State<BodyOrder> {
         /// Status order, irá sobrepor o [ListViewOrder], com o as informações da order
         Visibility(
           visible: _visible,
-          child: const StatusOrder(),
+          child: _infoOrder(),
         )
       ],
     );
   }
 
-  /// [ListViewOrder]
+  /// [ListViewOrder] lista de order
   Widget _listViewOrder(BuildContext context) {
     final OrderStore store = Modular.get();
     final Size screen = MediaQuery.of(context).size;
     return SizedBox(
       width: screen.width,
-      height: screen.height * 0.7,
+      height: screen.height * 0.6,
       child: ListView.builder(
         itemCount: store.order.length,
         itemBuilder: (context, index) {
@@ -100,7 +107,7 @@ class _BodyOrderState extends State<BodyOrder> {
     );
   }
 
-  /// [CardOrder]
+  /// [CardOrder] card da order
   Widget _cardOrder(BuildContext context, int index, OrderStore store) {
     final Size screen = MediaQuery.of(context).size;
     return GestureDetector(
@@ -108,9 +115,7 @@ class _BodyOrderState extends State<BodyOrder> {
         child: InkWell(
           splashColor: Colors.grey,
           onTap: () {
-            setState(() {
-              _setVisible();
-            });
+            _setVisible();
           },
           child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -180,18 +185,18 @@ class _BodyOrderState extends State<BodyOrder> {
   /// Mostra o status do pedido, aqui é definido o status
   Widget _statusOrder(OrderStore store, int index) {
     String status = store.order.elementAt(index).status;
-    Color cor_status = Colors.white;
+    Color corStatus = Colors.white;
     if (status == 'Em preparo') {
-      cor_status = Colors.amber;
+      corStatus = Colors.amber;
     } else if (status == 'A caminho') {
-      cor_status = Colors.blue;
+      corStatus = Colors.blue;
     } else if (status == 'Entregador chogou') {
-      cor_status = Colors.green;
+      corStatus = Colors.green;
     }
-
+  
     return Container(
       decoration: BoxDecoration(
-          color: cor_status, borderRadius: BorderRadius.circular(10)),
+          color: corStatus, borderRadius: BorderRadius.circular(10)),
       padding: const EdgeInsets.all(5),
       child: Text(
         status,
@@ -200,40 +205,130 @@ class _BodyOrderState extends State<BodyOrder> {
     );
   }
 
-
   /// [emptyOrder], caso não tenhsa pedidos
-  Widget _emptyOrder(){
+  Widget _emptyOrder() {
     final Size screen = MediaQuery.of(context).size;
     return Center(
       child: Column(
         children: [
-          const Text('Você ainda não pediu',
+          const Text(
+            'Você ainda não pediu',
             style: TextStyle(
               fontFamily: 'Nunito',
             ),
           ),
           const Text(
             'Que tal conhecer as melhores opções na sua região?',
-            style: TextStyle(
-              fontFamily: 'Nunito',
-              color: tertiaryCollor
-            ),
+            style: TextStyle(fontFamily: 'Nunito', color: tertiaryCollor),
           ),
           TextButton(
-            onPressed: () => Modular.to.navigate('/home'), 
-            child: const Text(
-              'Ir para o início', 
-              style: TextStyle(
-                fontSize: 20,
-                fontFamily: 'Nunito',
-                fontWeight: FontWeight.bold,
-                color: secondaryCollor
-              ),
-            )
-          )
-          
+              onPressed: () => Modular.to.navigate('/'),
+              child: const Text(
+                'Ir para o início',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.bold,
+                    color: secondaryCollor),
+              ))
         ],
       ),
+    );
+  }
+
+  /// [InfoOrder] page que mostará os detalhes do pedido
+  Widget _infoOrder() {
+    final Size screen = MediaQuery.of(context).size;
+    return Expanded(
+      child: Container(
+        width: screen.width,
+        height: screen.height,
+        color: primaryCollor,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: primaryCollor,
+                width: screen.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Observer(builder: (_) {
+                      return IconButton(
+                          onPressed: () {
+                            _setVisible();
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back_ios,
+                            color: secondaryCollor,
+                          ));
+                    }),
+                    const Text(
+                      'Detalhes do pedido',
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Container() // Não deleta, esse container ta servindo para centralizar o texto
+                  ],
+                ),
+              ),
+              _statusOrderinfo(),
+
+              SizedBox(height: screen.height * 0.02,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    // Container p/ as informações do pedido
+                    width: screen.width * 0.55,
+                    height: screen.height * 0.5,
+                    decoration: _containerDecoration(),
+                  ),
+
+                  Container(
+                    // Container p/ ficar os botões
+                    width: screen.width * 0.4,
+                    height: screen.height * 0.5,
+                    decoration: _containerDecoration(),
+                  ),
+
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// [Divisor dos Status]
+  Widget _statusOrderinfo(){
+    final Size screen = MediaQuery.of(context).size;
+    return Container(
+      width: screen.width,
+      height: screen.height * 0.2,
+      decoration: _containerDecoration()
+    );
+  }
+
+  /// [BoxShadow] dos container divisores
+  BoxDecoration _containerDecoration(){
+    return BoxDecoration(
+      color: primaryCollor,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black54,
+          blurRadius: 7,
+          offset: Offset(0, 3)
+        )
+      ]
     );
   }
 }
--- lib/app/modules/order/bodyOrder_page/status_order.dart
+++ lib/app/modules/order/bodyOrder_page/status_order.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pscomidas/app/modules/home/schemas.dart';
 
 class StatusOrder extends StatelessWidget {
-  const StatusOrder({ Key? key }) : super(key: key);
+  const StatusOrder({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size screen = MediaQuery.of(context).size;
     return Expanded(
       child: Container(
-        color: Colors.red,
-        width: 500, height: 500,
-        child: Text('oi container'),
+        width: screen.width,
+        height: screen.height,
+        color: primaryCollor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: (){
+                
+              },
+              icon: const Icon(Icons.arrow_back_ios, color: tertiaryCollor,)
+            )
+          ],
+        ),
       ),
     );
   }
