e83525f52a5ecbd6dac54831d4fd273230b78d6d
milena975 <milena.rios@mail.uft.edu.br>
2021-10-24 14:46:31 -0300

    R09/T02/feature/side_bar | Componentização dos elementos - criei a pasta "side_bar" em global/widgets - dentro da pasta side_bar, há uma pasta "components_side_bar" e dois arquivos: o primeiro contem as classes referentes aos botões; - no segundo, os métodos são referenciados passando o Text dos botões

---
+++ lib/app/global/widgets/side_bar/components_side_bar/components_side_bar.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:pscomidas/app/modules/home/schemas.dart';
+
+class LogoSideBar extends StatelessWidget {
+  const LogoSideBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screen = MediaQuery.of(context).size;
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        child: Image.asset(
+          "assets/images/logo-primary.png",
+          width: screen.width * 0.1,
+        ),
+        onTap: () => Navigator.popUntil(context, ModalRoute.withName('/')),
+      ),
+    );
+  }
+}
+
+class TextButtonMenu extends StatelessWidget {
+  final String option;
+  const TextButtonMenu({Key? key, required this.option}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        child: Text(
+          option,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontFamily: "Nunito",
+            fontWeight: FontWeight.bold,
+          ),
+          //textAlign: TextAlign.left,
+        ),
+      ),
+    );
+  }
+}
+
+class ListTilePerfil extends StatelessWidget {
+  const ListTilePerfil({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      //Aqui será a imagem do Upload (icon de demonstração)
+      leading: const Icon(
+        Icons.account_circle_sharp,
+        color: Colors.white,
+        size: 50,
+      ),
+      title: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          child: const Text(
+            "Editar perfil",
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: "Nunito",
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ),
+      minLeadingWidth: 0,
+    );
+  }
+}
+
+class ClosedButtonShop extends StatelessWidget {
+  const ClosedButtonShop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        // ignore: prefer_const_constructors
+        child: Text(
+          "Fechar loja",
+          style: const TextStyle(
+            color: secondaryCollor,
+            fontSize: 20,
+            fontFamily: "Nunito",
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/restaurant_home/restaurant_home_page.dart
+++ lib/app/modules/restaurant_home/restaurant_home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
+import 'package:pscomidas/app/global/widgets/side_bar/components_side_bar/menu_side_bar.dart';
+import 'package:pscomidas/app/modules/home/schemas.dart';
 import 'package:pscomidas/app/modules/restaurant_home/restaurant_home_store.dart';
-import 'package:pscomidas/app/modules/restaurant_home/side_bar_components/menu_side_bar.dart';
 
 class RestaurantHomePage extends StatefulWidget {
   final String title;
@@ -23,13 +24,24 @@ class RestaurantHomePageState extends State<RestaurantHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //side bar
-            Container(
+            SizedBox(
               width: screen.width * 0.2,
               child: const SideBar(),
             ),
             //main body
             Expanded(
-              child: Container(),
+              // ignore: avoid_unnecessary_containers
+              child: Container(
+                child: const Text(
+                  "Produtos",
+                  style: TextStyle(
+                    color: secondaryCollor,
+                    fontFamily: "Nunito",
+                    fontSize: 40,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ],
         ),
