035108e6cbb2b48c962269430c54defa5a2f3e8c
Edmundo Paulino <manyymoore@gmail.com>
2021-10-24 11:16:11 -0300

    R09/T05/feature/logout_button | Configuração
    Configurar desenvolvimento.
    **Desfazer antes do pull request.**

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -11,6 +11,7 @@ import 'modules/restaurant/restaurant_module.dart';
 import 'package:pscomidas/app/modules/cart/cart_module.dart';
 import 'package:pscomidas/app/modules/home/home_module.dart';
 import 'package:pscomidas/app/modules/restaurant/restaurant_module.dart';
+import 'package:pscomidas/app/modules/restaurant_home/restaurant_home_module.dart';
 
 import 'modules/register/register_module.dart';
 
@@ -26,7 +27,7 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: RestaurantHomeModule()),
     ModuleRoute(RestaurantModule.routeName, module: RestaurantModule()),
     ModuleRoute(CartModule.routeName, module: CartModule()),
     ModuleRoute(RegisterModule.routeName, module: RegisterModule()),
