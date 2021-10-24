ee765197abef3fa0aea7bf8444da749f4c3a09a8
milena975 <milena.rios@mail.uft.edu.br>
2021-10-23 22:54:06 -0300

    R09/T02/feature/side_bar | Criação do sideBar - criação do sideBar; - logo; - criação da classe para os textButtons

--- lib/app/modules/restaurant_home/restaurant_home_page.dart
+++ lib/app/modules/restaurant_home/restaurant_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:pscomidas/app/modules/restaurant_home/restaurant_home_store.dart';
+import 'package:pscomidas/app/modules/restaurant_home/side_bar_components/menu_side_bar.dart';
 
 class RestaurantHomePage extends StatefulWidget {
   final String title;
@@ -11,16 +12,27 @@ class RestaurantHomePage extends StatefulWidget {
 }
 
 class RestaurantHomePageState extends State<RestaurantHomePage> {
-  final RestaurantHomeStore store = Modular.get();
+  //final RestaurantHomeStore store = Modular.get();
 
   @override
   Widget build(BuildContext context) {
+    final Size screen = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: const <Widget>[],
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //side bar
+            Container(
+              width: screen.width * 0.2,
+              child: const SideBar(),
+            ),
+            //main body
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
       ),
     );
   }
