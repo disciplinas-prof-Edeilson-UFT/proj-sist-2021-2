4b29611884e741295a92e5756af57d253a31cbb7
thaisclxt <thais.calixto@mail.uft.edu.br>
2021-10-24 17:37:56 -0300

    R09/T04/feature/product_page | Implementação do card do produto
    - Movi o SingleChildScrollView para body
    - Apaguei a função de responsividade
    - Criei um Wrap com os cards
    - Corrigi o overflow
    - Mudei as cores dos cards
    - Adicionei título, descrição, preço e imagem ao card do produto

--- lib/app/modules/restaurant_home/components/product_card.dart
+++ lib/app/modules/restaurant_home/components/product_card.dart
@@ -1,38 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:pscomidas/app/global/utils/format_money.dart';
 import 'package:pscomidas/app/global/utils/schemas.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({Key? key, required this.image}) : super(key: key);
-  final String image;
+  const ProductCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: Image.asset(image, width: 250),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        child: SizedBox(
+          height: 200,
+          width: 400,
+          child: Stack(
+            children: [
+              const Positioned(
+                top: 10,
+                left: 10,
+                right: 150,
+                child: Text(
+                  'Pirulitos Halloween 30g',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Nunito',
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const Positioned(
+                top: 40,
+                left: 10,
+                right: 200,
+                child: Text(
+                  'Os pirulitos de chocolate ao leite e branco em formato de abobora e fantasma vão fazer sucesso no Hallowen! Feitos com chocolate ao leite e branco e decorados artesanalmente. Imagens ilustrativas.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'Nunito',
+                    color: tertiaryColor,
+                  ),
+                  maxLines: 4,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                child: Text(
+                  FormatMoney.doubleToMoney(5.90),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Nunito',
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                child: Image.asset(
+                  'assets/images/product_page/lolipop.webp',
+                  width: 180,
+                ),
+              ),
+            ],
+          ),
+        ),
+        onTap: () {},
+      ),
     );
   }
 }
 
 Widget addProduct() {
-  return Center(
-    child: Card(
-      color: primaryColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: InkWell(
-        onTap: () {},
-        child: const SizedBox(
-          width: 250,
-          height: 250,
-          child: Icon(
-            Icons.add_outlined,
-            color: secondaryColor,
-            size: 40,
-          ),
+  return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: InkWell(
+      borderRadius: BorderRadius.circular(10),
+      child: const SizedBox(
+        height: 200,
+        width: 400,
+        child: Icon(
+          Icons.add_outlined,
+          color: secondaryColor,
+          size: 40,
         ),
       ),
+      onTap: () {},
     ),
   );
 }
--- lib/app/modules/restaurant_home/components/product_page.dart
+++ lib/app/modules/restaurant_home/components/product_page.dart
@@ -6,63 +6,39 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "Produtos",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  overflow: TextOverflow.ellipsis,
-                  fontFamily: 'Nunito',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Produtos",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                    fontFamily: 'Nunito',
+                  ),
                 ),
               ),
             ),
-          ),
-          pageProductsResponsivity(width),
-        ],
-      ),
-    );
-  }
-}
-
-Widget pageProductsResponsivity(width) {
-  if (width > 850) {
-    return Center(
-      child: GridView.count(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        crossAxisCount: 5,
-        mainAxisSpacing: 1,
-        children: [
-          addProduct(),
-          ...List.filled(
-            9,
-            const ProductCard(
-              image: "assets/images/product_page/card_image.png",
+            Wrap(
+              spacing: 8,
+              children: [
+                addProduct(),
+                ...List.filled(
+                  20,
+                  const ProductCard(),
+                )
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        addProduct(),
-        ...List.filled(
-          9,
-          const ProductCard(image: "assets/images/product_page/card_image.png"),
-        )
-      ],
-    ),
-  );
 }
