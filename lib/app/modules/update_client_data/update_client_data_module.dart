02f218e3e44e3cc60adb3e5845bd06eda4fae2ed
Thaís Carvalho <thaiscarvalho115@gmail.com>
2021-11-07 19:30:06 -0300

    R10 | T04 | Verificação de novo número de telefone (#15)
    Implementação da verificação do número de telefone a partir do envio do código

--- lib/app/modules/update_client_data/update_client_data_module.dart
+++ lib/app/modules/update_client_data/update_client_data_module.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pscomidas/app/modules/register_client/register_client_repository.dart';
 
 import 'update_client_data_page.dart';
 import 'update_client_data_store.dart';
@@ -7,6 +9,7 @@ class ChangeClientDataModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => ChangeClientDataStore()),
+    Bind.lazySingleton((i) => RegisterClientRepository(FirebaseAuth.instance))
   ];
 
   @override
--- lib/app/modules/update_client_data/update_client_data_store.dart
+++ lib/app/modules/update_client_data/update_client_data_store.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:pscomidas/app/modules/register_client/register_client_repository.dart';
 
 part 'update_client_data_store.g.dart';
 
@@ -6,11 +10,33 @@ class ChangeClientDataStore = _ChangeClientDataStoreBase
     with _$ChangeClientDataStore;
 
 abstract class _ChangeClientDataStoreBase with Store {
+  final _register_repository = Modular.get<RegisterClientRepository>();
+
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController codeController = TextEditingController();
+
   @observable
-  int value = 0;
+  ConfirmationResult? confirmationResult;
+
+  @observable
+  bool? validatorPhone;
+  
+  @observable
+  String? errorPhone;
 
   @action
-  void increment() {
-    value++;
+  Future<void> sendVerifyCode() async {
+    confirmationResult =
+        await _register_repository.verifyNumberForWeb(phoneController.text);
+  }
+
+  @action
+  Future<void> verifyCode() async {
+    try {
+      validatorPhone = await _register_repository.verifyCodeForWeb(
+          confirmationResult, codeController.text);
+    } catch (e) {
+      errorPhone = e.toString();
+    }
   }
 }
