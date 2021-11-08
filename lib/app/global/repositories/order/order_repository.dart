a7c07a9c4e9a29856a70fac66115672d5c52e345
Victor Hugo C. Gomes <vhclaudiogomes@gmail.com>
2021-11-08 09:56:25 -0300

    feature/r07/t03/rating - Função reformulada - Função para avaliação do pedido reformulada para receber valores brutos e não uma classe.

--- lib/app/global/repositories/order/order_repository.dart
+++ lib/app/global/repositories/order/order_repository.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pscomidas/app/global/models/entities/order.dart';
-import 'package:pscomidas/app/global/models/entities/rating.dart';
 import 'package:pscomidas/app/global/models/enums/order_status.dart';
 import 'package:pscomidas/app/global/repositories/order/order_repository_interface.dart';
 
@@ -27,11 +26,11 @@ class OrderRepository implements IOrderRepository {
   }
 
   @override
-  Future<void> ratingOrder(Order pedido, Rating avaliacao) async {
+  Future<void> ratingOrder(Order pedido, String comentario, String nota) async {
     try {
       await firestore.collection('rating').add({
-        'comment': avaliacao.comentario,
-        'value': avaliacao.nota,
+        'comment': comentario,
+        'value': nota,
       }).then((value) {
         firestore
             .collection('order')
--- lib/app/global/repositories/order/order_repository_interface.dart
+++ lib/app/global/repositories/order/order_repository_interface.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pscomidas/app/global/models/entities/order.dart';
-import 'package:pscomidas/app/global/models/entities/rating.dart';
 
 abstract class IOrderRepository {
   Future<Order>? getOrder(Order pedido);
   Future<DocumentReference> cadastrarOrder(Order pedido);
-  Future<void> ratingOrder(Order pedido, Rating avaliacao);
+  Future<void> ratingOrder(Order pedido, String comentario, String nota);
 }
