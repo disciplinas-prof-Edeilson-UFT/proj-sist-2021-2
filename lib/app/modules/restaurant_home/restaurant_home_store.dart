f580570dff108630ee7c9d9499f82215d014684d
Victor Hugo C. Gomes <vhclaudiogomes@gmail.com>
2021-11-19 00:25:34 -0300

    feature/R15/t02/ - Adicionado campos - Adicionado campos no restaurant_home_store e a chamada da função cadastro de produto no banco.

--- lib/app/modules/restaurant_home/restaurant_home_store.dart
+++ lib/app/modules/restaurant_home/restaurant_home_store.dart
@@ -1,11 +1,19 @@
+import 'dart:developer';
+
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:pscomidas/app/global/models/entities/product.dart';
+import 'package:pscomidas/app/global/repositories/product/product_repository.dart';
 
 part 'restaurant_home_store.g.dart';
 
 class RestaurantHomeStore = _RestaurantHomeStoreBase with _$RestaurantHomeStore;
 
 abstract class _RestaurantHomeStoreBase with Store {
+  final ProductRepository productRepository = Modular.get<ProductRepository>();
+
   @action
   Future imageReceiver(dynamic e) async {
     if (e.type != 'image/jpeg' && e.type != 'image/png') {
@@ -18,6 +26,12 @@ abstract class _RestaurantHomeStoreBase with Store {
     }
   }
 
+  TextEditingController nameController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController categoriesController = TextEditingController();
+  TextEditingController idController = TextEditingController();
+
   @observable
   bool isOpen = false;
 
@@ -27,6 +41,12 @@ abstract class _RestaurantHomeStoreBase with Store {
   @action
   void toggleOpen() {
     isOpen = !isOpen;
+  }
+
+  @action
+  Future cadastrarProdutoTeste(Product produto) async {
+    await productRepository.cadastrarProduct(produto);
+    log('Deu certo');
   }
 
   @computed
