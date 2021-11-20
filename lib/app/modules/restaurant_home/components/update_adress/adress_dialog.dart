08b07ad8c509186be263dfe565a8787465d8328e
thaisclxt <thais.calixto@mail.uft.edu.br>
2021-11-19 21:33:21 -0300

    hotfix/R13/alert_dialog | Correção do arquivo `adress_dialog.dart` - Não tem necessidade da classe ser StatefulWidget, então converti para StatelessWidget - Correção de padding para ficar padronizado com as outras dialogs - Chamada de ConfirmationButton ao invés de reescrever exatamente o mesmo código - Adição correta de onPressed em ConfirmationButton

--- lib/app/modules/restaurant_home/components/update_adress/adress_dialog.dart
+++ lib/app/modules/restaurant_home/components/update_adress/adress_dialog.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:pscomidas/app/global/utils/schemas.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pscomidas/app/global/repositories/restaurant_home/profile/profile_repository.dart';
+import 'package:pscomidas/app/modules/restaurant_home/components/components_profile_dialog.dart';
 import 'package:pscomidas/app/modules/restaurant_home/components/update_adress/adress_formulary.dart';
+import 'package:pscomidas/app/modules/restaurant_home/restaurant_home_store.dart';
 
-class AdressDialog extends StatefulWidget {
-  const AdressDialog({Key? key}) : super(key: key);
+class AdressDialog extends StatelessWidget {
+  AdressDialog({Key? key}) : super(key: key);
 
-  @override
-  _AdressDialogState createState() => _AdressDialogState();
-}
+  final RestaurantHomeStore homeStore = Modular.get<RestaurantHomeStore>();
 
-class _AdressDialogState extends State<AdressDialog> {
   @override
   Widget build(BuildContext context) {
     double _pageWidth = MediaQuery.of(context).size.width;
     return AlertDialog(
-      contentPadding: const EdgeInsets.only(top: 24),
+      contentPadding: const EdgeInsets.only(top: 10),
       title: const Text('Endereço'),
       content: SingleChildScrollView(
         child: SizedBox(
@@ -24,26 +24,13 @@ class _AdressDialogState extends State<AdressDialog> {
               AdressFormulary(),
               Padding(
                 padding: const EdgeInsets.only(
-                    top: 30.0, bottom: 30.0, left: 15.0, right: 15.0),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: TextButton(
-                      child: const Text(
-                        'Confirmar cadastro',
-                        style: TextStyle(
-                          fontFamily: 'Nunito',
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        elevation: MaterialStateProperty.all(4),
-                        minimumSize:
-                            MaterialStateProperty.all(const Size(210, 48)),
-                        backgroundColor:
-                            MaterialStateProperty.all(secondaryColor),
-                      ),
-                      onPressed: () {}),
+                    top: 10.0, bottom: 10.0, left: 15.0, right: 15.0),
+                child: ConfirmationButton(
+                  onPressed: () async {
+                    await ProfileRepository()
+                        .setRestaurant(homeStore.restaurant!);
+                    Navigator.of(context).pop();
+                  },
                 ),
               ),
             ],
