b11433dfe59c2213c760e4f2b020149cec48cfe1
Max Alexandre <max.alexandre839@gmail.com>
2021-11-21 00:16:29 -0300

    R11 | T05 | Tela-Pesquisa
    - Pequena mudança no estilo da primeira página.

--- lib/app/modules/client_address/pages/saved_addresses.dart
+++ lib/app/modules/client_address/pages/saved_addresses.dart
@@ -19,45 +19,42 @@ class _SavedAdressesState extends State<SavedAdresses> {
   @override
   Widget build(BuildContext context) {
     Size screen = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: () => store.jump(1),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: screen.height * .3,
-              width: screen.width * .3,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.contain,
-                  image: NetworkImage('https://i.imgur.com/50wsQ3L.jpg'),
-                ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: screen.height * .3,
+            width: screen.width * .3,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: NetworkImage('https://i.imgur.com/50wsQ3L.jpg'),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                "Onde você quer receber seu pedido?",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
-                ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Onde você quer receber seu pedido?",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
               ),
             ),
-            fakeSearch(),
-            const SizedBox(
-              height: 10,
-            ),
-            ListView.builder(
-              itemBuilder: (context, index) {
-                return const AddressListTile();
-              },
-              shrinkWrap: true,
-              itemCount: test.length,
-            ),
-          ],
-        ),
+          ),
+          fakeSearch(),
+          const SizedBox(
+            height: 10,
+          ),
+          ListView.builder(
+            itemBuilder: (context, index) {
+              return const AddressListTile();
+            },
+            shrinkWrap: true,
+            itemCount: test.length,
+          ),
+        ],
       ),
     );
   }
